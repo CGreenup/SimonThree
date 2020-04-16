280449bdaec7d1136db873c21213fd2eb780661f
cgreenup <charlirejunk@gmail.com>
2020-04-16 15:39:01 -0500

    Game Board and Game Info activities created Added light-state images

--- app/src/main/java/com/chrisgreenup/simonthree/MainActivity.java
+++ app/src/main/java/com/chrisgreenup/simonthree/MainActivity.java
@@ -3,12 +3,20 @@ package com.chrisgreenup.simonthree;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
