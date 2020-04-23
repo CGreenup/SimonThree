02fccf7ca18e11dec49c50d7362fdac455abdf7f
cgreenup <charlirejunk@gmail.com>
2020-04-23 01:52:03 -0500

    Added combo-break/fail sound

--- app/src/main/java/com/chrisgreenup/simonthree/Game.java
+++ app/src/main/java/com/chrisgreenup/simonthree/Game.java
@@ -29,7 +29,7 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Game extends AppCompatActivity
-implements View.OnClickListener {
+        implements View.OnClickListener {
 
     private static Intent intent;
 
@@ -52,21 +52,22 @@ implements View.OnClickListener {
     private int currentscore;
 
     //Enum of all of the colors, and a method to get a random color
-    private enum buttonCommands{
+    private enum buttonCommands {
         RED, YELLOW, GREEN, BLUE;
-        private static buttonCommands [] buttons = values();
-        public buttonCommands next(){
+        private static buttonCommands[] buttons = values();
+
+        public buttonCommands next() {
             Random rand = new Random();
-            return buttons[(this.ordinal() + rand.nextInt(buttons.length)) % 4];
+            return buttons[( this.ordinal() + rand.nextInt(buttons.length) ) % 4];
         }
     }
 
     private buttonCommands button = buttonCommands.RED;
 
     //Variables of imagebuttons to let Simon flash and beep them to signal the player
-    private ImageButton imageButtonRed ;
-    private ImageButton imageButtonYellow ;
-    private ImageButton imageButtonGreen ;
+    private ImageButton imageButtonRed;
+    private ImageButton imageButtonYellow;
+    private ImageButton imageButtonGreen;
     private ImageButton imageButtonBlue;
 
 
@@ -78,6 +79,7 @@ implements View.OnClickListener {
     int beep2Id;
     int beep3Id;
     int beep4Id;
+    int failId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,7 +87,7 @@ implements View.OnClickListener {
         setContentView(R.layout.game_board);
         soundsLoaded = new HashSet<Integer>();
 
-        intent = getIntent();
+        intent   = getIntent();
         gameMode = intent.getStringExtra("game");
 
         currentscore = 0;
@@ -95,8 +97,8 @@ implements View.OnClickListener {
 
 
         String id = "simon_button_";
-        for (int i = 0; i < 4; i++){
-            int resID = getResources().getIdentifier(id + i, "id", getPackageName());
+        for (int i = 0; i < 4; i++) {
+            int resID = getResources().getIdentifier(id + i , "id" , getPackageName());
             ImageButton imageButton = findViewById(resID);
             imageButton.setImageResource(R.drawable.light_off);
             imageButton.setOnClickListener(this);
@@ -110,42 +112,42 @@ implements View.OnClickListener {
 
     //Initializes the instance variables of the imageButtons
     //Doubles in function to change the buttons gray if the game mode is Simon: Surprise
-    void setupExtraButton(String gameMode){
-        imageButtonRed = findViewById(R.id.simon_button_0);
+    void setupExtraButton(String gameMode) {
+        imageButtonRed    = findViewById(R.id.simon_button_0);
         imageButtonYellow = findViewById(R.id.simon_button_1);
-        imageButtonGreen = findViewById(R.id.simon_button_2);
-        imageButtonBlue = findViewById(R.id.simon_button_3);
+        imageButtonGreen  = findViewById(R.id.simon_button_2);
+        imageButtonBlue   = findViewById(R.id.simon_button_3);
 
-        if(gameMode.equals("surprise")){
+        if (gameMode.equals("surprise")){
             imageButtonBlue.setBackgroundColor(0xff818181);
             imageButtonRed.setBackgroundColor(0xff818181);
             imageButtonYellow.setBackgroundColor(0xff818181);
             imageButtonGreen.setBackgroundColor(0xff818181);
         }
-        else {
-            imageButtonRed.setBackgroundColor(getResources().getColor( R.color.simonRed));
-            imageButtonYellow.setBackgroundColor(getResources().getColor( R.color.simonYellow));
-            imageButtonGreen.setBackgroundColor(getResources().getColor( R.color.simonGreen));
-            imageButtonBlue.setBackgroundColor(getResources().getColor( R.color.simonBlue));
+        else{
+            imageButtonRed.setBackgroundColor(getResources().getColor(R.color.simonRed));
+            imageButtonYellow.setBackgroundColor(getResources().getColor(R.color.simonYellow));
+            imageButtonGreen.setBackgroundColor(getResources().getColor(R.color.simonGreen));
+            imageButtonBlue.setBackgroundColor(getResources().getColor(R.color.simonBlue));
         }
     }
 
     @Override
     public void onClick(View view) {
-        if(playersTurn){
+        if (playersTurn){
             buttonCommands currentColor;
 
             switch (view.getId()){
-                case (R.id.simon_button_0):
+                case ( R.id.simon_button_0 ):
                     currentColor = buttonCommands.RED;
                     break;
-                case (R.id.simon_button_1):
+                case ( R.id.simon_button_1 ):
                     currentColor = buttonCommands.YELLOW;
                     break;
-                case (R.id.simon_button_2):
+                case ( R.id.simon_button_2 ):
                     currentColor = buttonCommands.GREEN;
                     break;
-                case (R.id.simon_button_3):
+                case ( R.id.simon_button_3 ):
                     currentColor = buttonCommands.BLUE;
                     break;
                 default:
@@ -153,7 +155,7 @@ implements View.OnClickListener {
                     break;
             }
 
-            Log.i("DSIMONSAYS", currentColor.toString());
+            Log.i("DSIMONSAYS" , currentColor.toString());
 
             //Make the button pressed beep
             //soundsLoaded = new HashSet<Integer>();
@@ -166,7 +168,7 @@ implements View.OnClickListener {
             playBeep(currentColor);
 
             //If the player has entered the correct sequence
-            if(pickedTheRightColor(currentColor)){
+            if (pickedTheRightColor(currentColor)){
                 if (index == history.size()){
                     updateScores();
                     SimonSay ss = new SimonSay();
@@ -181,7 +183,7 @@ implements View.OnClickListener {
 
     }
 
-    private void updateScores(){
+    private void updateScores() {
         currentscore = history.size();
 
         if (currentscore > highscore){
@@ -200,72 +202,77 @@ implements View.OnClickListener {
         @Override
         protected Void doInBackground(final ImageButton... imageButtons) {
 
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        imageButtons[0].setImageResource(R.drawable.light_on);
-                    }
-                });
-
-                try {
-                    Thread.sleep(200);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    imageButtons[0].setImageResource(R.drawable.light_on);
                 }
+            });
 
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        imageButtons[0].setImageResource(R.drawable.light_off);
-                    }
-                });
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    imageButtons[0].setImageResource(R.drawable.light_off);
+                }
+            });
 
 
             return null;
         }
     }
 
-    private void playBeep(buttonCommands color){
+    private void playBeep(buttonCommands color) {
 
         if (!gameMode.equals("surprise")){
             if (color.equals(button.RED)){
                 if (soundsLoaded.contains(beep1Id)){
-                    soundPool.play(beep1Id, 1.0f, 1.0f,
-                            0, 0, 1.0f);
+                    soundPool.play(beep1Id , 1.0f , 1.0f ,
+                            0 , 0 , 1.0f);
                 }
             }
             else if (color.equals(button.YELLOW)){
                 if (soundsLoaded.contains(beep2Id)){
-                    soundPool.play(beep2Id, 1.0f, 1.0f,
-                            0, 0, 1.0f);
+                    soundPool.play(beep2Id , 1.0f , 1.0f ,
+                            0 , 0 , 1.0f);
                 }
             }
             else if (color.equals(button.GREEN)){
                 if (soundsLoaded.contains(beep3Id)){
-                    soundPool.play(beep3Id, 1.0f, 1.0f,
-                            0, 0, 1.0f);
+                    soundPool.play(beep3Id , 1.0f , 1.0f ,
+                            0 , 0 , 1.0f);
                 }
             }
             else if (color.equals(button.BLUE)){
                 if (soundsLoaded.contains(beep4Id)){
-                    soundPool.play(beep4Id, 1.0f, 1.0f,
-                            0, 0, 1.0f);
+                    soundPool.play(beep4Id , 1.0f , 1.0f ,
+                            0 , 0 , 1.0f);
                 }
             }
         }
         else{
             if (soundsLoaded.contains(beep3Id)){
-                soundPool.play(beep3Id, 1.0f, 1.0f,
-                        0, 0, 1.0f);
+                soundPool.play(beep3Id , 1.0f , 1.0f ,
+                        0 , 0 , 1.0f);
             }
         }
 
     }
 
-    private void restartGame(){
+    private void restartGame() {
         if (history != null){
             history.clear();
-        }else{
+            if (soundsLoaded.contains(failId)){
+                soundPool.play(failId , 1.0f , 1.0f ,
+                        0 , 0 , 1.0f);
+            }
+        }
+        else{
             history = new ArrayList<>();
         }
 
@@ -273,11 +280,11 @@ implements View.OnClickListener {
         simonSay.execute();
     }
 
-    private boolean pickedTheRightColor(buttonCommands color){
+    private boolean pickedTheRightColor(buttonCommands color) {
         //If the player isn't playing Simon Rewind, use normal rules
         if (!gameMode.equals("rewind")){
             if (history.get(index).equals(color)){
-                Log.i("DSIMONDEBUG", "correct color, not rewind");
+                Log.i("DSIMONDEBUG" , "correct color, not rewind");
                 index++;
                 return true;
             }
@@ -285,8 +292,8 @@ implements View.OnClickListener {
         //otherwise, use rules that are backwards compared to Simon
         else{
             int size = history.size() - 1;
-            if(history.get(size-index).equals(color)){
-                Log.i("DSIMONDEBUG", "correct color, rewind");
+            if (history.get(size - index).equals(color)){
+                Log.i("DSIMONDEBUG" , "correct color, rewind");
                 index++;
                 return true;
             }
@@ -295,13 +302,13 @@ implements View.OnClickListener {
         return false;
     }
 
-    private void saveHighscore(){
+    private void saveHighscore() {
 
         FileOutputStream fos = null;
         try {
             String filename = gameMode + ".txt";
 
-            fos = openFileOutput(filename, Context.MODE_PRIVATE);
+            fos = openFileOutput(filename , Context.MODE_PRIVATE);
             OutputStreamWriter osw = new OutputStreamWriter(fos);
             BufferedWriter bw = new BufferedWriter(osw);
             PrintWriter pw = new PrintWriter(bw);
@@ -309,13 +316,12 @@ implements View.OnClickListener {
             pw.println(highscore);
 
             pw.close();
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    private String getSavedHighscore(){
+    private String getSavedHighscore() {
         FileInputStream fis = null;
         String filename = gameMode + ".txt";
         String output = getResources().getString(R.string.highscore_text) + " ";
@@ -337,7 +343,7 @@ implements View.OnClickListener {
             e.printStackTrace();
         }
 
-        return  output;
+        return output;
 
     }
 
@@ -358,20 +364,22 @@ implements View.OnClickListener {
 
         soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
             @Override
-            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+            public void onLoadComplete(SoundPool soundPool , int sampleId , int status) {
                 if (status == 0){
-                    Log.i("SOUND", "Sound loaded " + sampleId);
+                    Log.i("SOUND" , "Sound loaded " + sampleId);
                     soundsLoaded.add(sampleId);
-                }else{
-                    Log.i("SOUND", "ERROR, cannot load sound status = " + status);
+                }
+                else{
+                    Log.i("SOUND" , "ERROR, cannot load sound status = " + status);
                 }
             }
         });
 
-        beep1Id  = soundPool.load(this, R.raw.bleep1, 1);
-        beep2Id  = soundPool.load(this, R.raw.bleep2, 1);
-        beep3Id  = soundPool.load(this, R.raw.bleep3, 1);
-        beep4Id  = soundPool.load(this, R.raw.bleep4, 1);
+        beep1Id = soundPool.load(this , R.raw.bleep1 , 1);
+        beep2Id = soundPool.load(this , R.raw.bleep2 , 1);
+        beep3Id = soundPool.load(this , R.raw.bleep3 , 1);
+        beep4Id = soundPool.load(this , R.raw.bleep4 , 1);
+        failId  = soundPool.load(this , R.raw.fail , 1);
     }
 
     @Override
@@ -386,18 +394,16 @@ implements View.OnClickListener {
     }
 
     //Algorithm for controlling Simon, the opponent
-    class SimonSay extends AsyncTask<Void, Void, Void>{
+    class SimonSay extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... voids) {
             playersTurn = false;
             history.add(button.next());
 
-
-            Log.i("DSIMONHISTORY", "_____________________");
+            Log.i("DSIMONHISTORY" , "_____________________");
             for (int i = 0; i < history.size(); i++) {
-                Log.i("DSIMONHISTORY", history.get(i).toString());
+                Log.i("DSIMONHISTORY" , history.get(i).toString());
             }
-
 
 
             try {
@@ -405,8 +411,8 @@ implements View.OnClickListener {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            Log.i("DSIMONDEBUG", "history size = " + history.size());
-            for(int i = 0; i < history.size(); i++) {
+            Log.i("DSIMONDEBUG" , "history size = " + history.size());
+            for (int i = 0; i < history.size(); i++) {
                 try {
                     playBeep(history.get(i));
                     buttonFlash(getCorrespondingButton(history.get(i)));
@@ -419,7 +425,7 @@ implements View.OnClickListener {
             return null;
         }
 
-        private void buttonFlash(final ImageButton button){
+        private void buttonFlash(final ImageButton button) {
 
             runOnUiThread(new Runnable() {
                 @Override
@@ -444,7 +450,7 @@ implements View.OnClickListener {
 
         }
 
-        private ImageButton getCorrespondingButton(buttonCommands color){
+        private ImageButton getCorrespondingButton(buttonCommands color) {
             if (color.equals(buttonCommands.RED))
                 return imageButtonRed;
             else if (color.equals(buttonCommands.BLUE))
@@ -454,7 +460,7 @@ implements View.OnClickListener {
             else if (color.equals(buttonCommands.GREEN))
                 return imageButtonGreen;
             else{
-                Log.i("DSIMONHISTORY", "ERROR WITH getCorrespondingButton");
+                Log.i("DSIMONHISTORY" , "ERROR WITH getCorrespondingButton");
                 return imageButtonRed;
             }
         }
@@ -464,7 +470,7 @@ implements View.OnClickListener {
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
             playersTurn = true;
-            index = 0;
+            index       = 0;
         }
     }
 }
